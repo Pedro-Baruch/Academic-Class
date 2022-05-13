8473e6b74a889a630ef7e8cc917382e64c564261
Pedro-Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2022-05-13 19:41:15 -0300

    organizando

---
+++ frontend-ac/src/Registro/Registro.jsx
@@ -0,0 +1,53 @@
+import { firebaseApp } from "../App"
+import { addDoc, collection, deleteDoc, doc, getDocs, getFirestore } from "firebase/firestore"
+import { useEffect, useState } from "react"
+
+export const Registro = () => {
+    
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+    const [users, setUsers] = useState([])
+
+    const db = getFirestore(firebaseApp)
+    const userCollectionRef = collection(db, "users")
+
+    async function criarUser() {
+        const user = await addDoc(userCollectionRef, {name, email, senha})
+    }
+
+    useEffect(() => {
+        const getUsers = async () => {
+            const data = await getDocs(userCollectionRef)
+            setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })))
+        };
+
+        getUsers()
+    },[])
+
+    /*async function deleteUser(id) {
+        const userDoc = doc(db, "users", id)
+        await deleteDoc(userDoc)
+    }*/
+
+    return(
+        <div>
+            <input type="text" 
+            placeholder="Nome" 
+            value={name} 
+            onChange={e => setName(e.target.value)} 
+            />
+            <input type="text" 
+            placeholder="Email" 
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+            />
+            <input type="text" 
+            placeholder="Senha"
+            value={senha}
+            onChange={e => setSenha(e.target.value)}
+            />
+            <button onClick={criarUser}>Criar User</button>
+        </div>
+    )
+}
--- frontend-ac/src/index.jsx
+++ frontend-ac/src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { Registro } from './Registro/Registro.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Registro />
   </React.StrictMode>
 );
