ef626804ca614ce2dcfee0b462dde27a6e9bd557
Pedro-Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2022-05-21 22:11:22 -0300

    inserindo html de atividades

--- academic/src/pages/Atividades.js
+++ academic/src/pages/Atividades.js
@@ -4,12 +4,19 @@ export function Atividades(){
 
 
     return(
-        <div>
+        <div className='atividades-turma'>
             <nav class="main-nav">
                 <a href="http://localhost:3000/home/post">Publicações</a>
                 <a href="http://localhost:3000/home/pessoas">Pessoas</a>
                 <a href="http://localhost:3000/home/atividade">Atividades</a>
             </nav>
+            <div className='atividades'>
+                <h2 className='atividades-titulo'>Atividades</h2>
+                <ul className='atividades-objeto'>
+                    <li>Nome</li>
+                    <li>Data de entrega</li>
+                </ul>
+            </div>
         </div>
     )
 }
