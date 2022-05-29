d1ce88cce4b1104b104a5a9d554b919655b9cc45
Pedro-Baruch <84157648+Pedro-Baruch@users.noreply.github.com>
2022-05-29 18:14:13 -0300

    concertando imagem

--- academic/src/pages/Registro.jsx
+++ academic/src/pages/Registro.jsx
@@ -22,16 +22,18 @@ export const Registro = () => {
   }
 
   return (
-    <div className='container-reg'>
-      <ul className='sobre'>
-        <li className='titulo-sobre'>Sobre Academic Class</li>
-        <li className='texto-sobre'>Em meio às constantes prevenção adotadas para combater o vírus COVID-19, surgiu a dificuldade de ocorrer aulas presenciais e isso se tornou um desafio para a continuação da educação nas instituições de ensino. </li>
-        <li className='texto-sobre'>Nesse sentido, o Academic Class foi projetado para auxiliar professores, alunos e escolas a simularem ensinamentos presenciais, proporcionando às turmas comunicar-se e manter as aulas a distância mais organizadas.</li>
-      </ul>
+    <div className='imagem-fundo'>
+      <div className='container-reg'>
+        <ul className='sobre'>
+          <li className='titulo-sobre'>Sobre Academic Class</li>
+          <li className='texto-sobre'>Em meio às constantes prevenção adotadas para combater o vírus COVID-19, surgiu a dificuldade de ocorrer aulas presenciais e isso se tornou um desafio para a continuação da educação nas instituições de ensino. </li>
+          <li className='texto-sobre'>Nesse sentido, o Academic Class foi projetado para auxiliar professores, alunos e escolas a simularem ensinamentos presenciais, proporcionando às turmas comunicar-se e manter as aulas a distância mais organizadas.</li>
+        </ul>
       <form className='google-reg' onSubmit= { handleSubmit }>
         <img className='google-icon' src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/google/google-original.svg" />
         <button className='button-reg'>Faça registro pelo google</button>
       </form>
+    </div>
     </div>
   )
 }
