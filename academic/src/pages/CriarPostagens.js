ff67094a1a32f71dda0b0012448129d2674f8319
jossuel-js <jossueltherock@gmail.com>
2022-05-31 05:19:08 -0300

    interação-postagem

--- academic/src/pages/CriarPostagens.js
+++ academic/src/pages/CriarPostagens.js
@@ -1,6 +1,59 @@
 import './../pages/RegistroStyle.css';
+import { addDoc, collection, deleteDoc, doc, getDocs, getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react';
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyBeUZICL0_YOZXyjMCXqEn4JTRjCBF9G5k",
+    authDomain: "registro-591d2.firebaseapp.com",
+    projectId: "registro-591d2",
+  
+  });
 
 export function CriarPost(){
+
+    const navigate = useNavigate()
+
+    const db = getFirestore(firebaseApp);
+    const usercollectionRef = collection(db,'/criar/3IPKXVngxru1ut5YCQsC/posts')
+
+    const [title, setTitle] = useState('');
+    const [descrição, setDescrição] = useState('');
+    const [users, SetUser] = useState('');
+    
+  
+   
+  
+    useEffect(() => {
+      const getUsers = async () => {
+        const data = await getDocs(usercollectionRef)
+  
+        SetUser(console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))))
+      };
+      getUsers();
+    }, []);
+  
+  
+    const handleChange = (e) => {
+      setTitle(e.target.value);
+    }
+  
+    const handleDescriçãoChange = (e) => {
+      setDescrição(e.target.value);
+    }
+
+    async function CriarPost() {
+  
+        const user = await addDoc(usercollectionRef, {
+          title,
+          descrição,
+    
+        });
+        console.log(user);
+        navigate('/home/post')
+        
+      }
 
 
     return(
@@ -8,9 +61,12 @@ export function CriarPost(){
             <form>
                 <h1>Criar Post</h1>
                 <label>Titulo: </label>
-                <input></input>
+                <input value={title}
+            required onChange={(e) => { handleChange(e) }} /><br />
                 <label>descrição: </label>
-                <input></input>
+                <input  value={descrição}
+            required onChange={(e) => { handleDescriçãoChange(e) }} /><br />
+                <button onClick={CriarPost}>Criar</button>
             </form>
         </div>
     )
--- academic/src/pages/Postagens.js
+++ academic/src/pages/Postagens.js
@@ -1,8 +1,38 @@
 import './../pages/RegistroStyle.css';
+import ReactDOM from 'react-dom';
+import React, { useEffect, useState } from 'react';
+import { initializeApp } from "firebase/app"
+import { addDoc, collection, deleteDoc, doc, getDocs, getFirestore } from 'firebase/firestore';
+import { async } from '@firebase/util';
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyBeUZICL0_YOZXyjMCXqEn4JTRjCBF9G5k",
+    authDomain: "registro-591d2.firebaseapp.com",
+    projectId: "registro-591d2",
+  
+  });
 
 export function Post(){
+    
 
+    const db = getFirestore(firebaseApp);
+    const usersCollectionRef = collection(db,'/criar/3IPKXVngxru1ut5YCQsC/posts')
 
+    const [title, setTitle] = useState('');
+    const [descrição, setDescrição] = useState('');
+    const [users, setUsers] = useState('');
+    
+    useEffect(() => {
+        const getUsers = async () => {
+          const data = await getDocs(usersCollectionRef);
+          setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        };
+        getUsers();
+      }, []);
+    
+    
+  
+       
     return(
         <div className='posts-turma'>
             <nav className="main-nav">
@@ -23,10 +53,30 @@ export function Post(){
                     <h2 className="nome-user">NomeUser</h2>
                     <p>dataPost</p>
                     <div class="postagem">
-                        <p>Postagem</p>
+                        <p>asdfasfsafsafsfsfs</p>
                     </div>
                 </div>    
             </div>
+
+            {users.map((user) => {
+              return (
+                <>
+                  
+                  <div className='post'>
+                    <img className='imagem-perfil-post' src=""></img>
+                    <h2 className="nome-user">NomeUser</h2>
+                    <p>dataPost</p>
+                    <div class="postagem">
+                       {user.descrição}
+                    </div>
+                </div>    
+                  
+
+                </>
+              );
+            })}
+            
         </div>
     )
+
 }
