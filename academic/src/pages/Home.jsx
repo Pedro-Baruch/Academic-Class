59bf427b13eb489196dfa0ced3e7e60e0dd26e51
Ianara-cs <ianarasilva333@gmail.com>
2022-06-04 20:29:09 -0300

    Verificação se turma existe e se o user já participa da turma

--- academic/src/pages/Home.jsx
+++ academic/src/pages/Home.jsx
@@ -1,6 +1,6 @@
-import { collection, deleteDoc, doc, getDocs } from 'firebase/firestore';
+import { collection, doc, getDocs, onSnapshot } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { CardTurmaItem } from '../componentes/CardTurmaItem';
 import { ColunaTurmas } from '../componentes/ColunaTurmas';
 import { Footer } from '../componentes/Footer';
@@ -11,11 +11,12 @@ import './../pages/RegistroStyle.css';
 
 
 export const Home = () => {
-  const navigate = useNavigate();
-  const [name, setName] = useState("");
-  const [descrição, setDescrição] = useState("");
+  const { currentUser } = useAuth()
   const [turmas, setTurmas] = useState([]);
-  const {currentUser, signup} = useAuth()
+  const [codTurma, setCodTurma] = useState([]);
+  const [pessoas, setPessoas] = useState([]);
+  const [admin, setAdmin] = useState([]);
+
 
   const usersCollectionRef = collection(db, "turma");
 
@@ -28,9 +29,72 @@ export const Home = () => {
     return getUsers
   }, []);
 
-  async function deleteUser(id) {
-    const userDoc = doc(db, "users", id);
-    await deleteDoc(userDoc);
+  function procurarTurma(cod) {
+    const turmaEncontrada = []
+    turmas.map((turma) => {
+      if(turma.id === cod){
+        turmaEncontrada.push(turma.id)
+      }
+      return 
+    })
+    return turmaEncontrada[0]
+    
+
+  }
+
+  function procurarPessoa() {
+    const pessoaEncontrada = []
+
+    const tur =  procurarTurma(codTurma)
+    onSnapshot(doc(db, 'turma', `${tur}`), (doc) => {
+      setPessoas(doc.data()?.users)
+      setAdmin(doc.data()?.admin)
+    })
+    
+    pessoas.map((user) => {
+      if(user.id === currentUser?.id){
+        pessoaEncontrada.push(user.id)
+      }
+      return 
+    })
+    
+    admin.map((user) => {
+      if(user.id === currentUser?.id){
+        pessoaEncontrada.push(user.id)
+      }
+      return 
+    })
+    
+    return pessoaEncontrada
+
+  }
+
+  function confirmarTurma() {
+    const t = procurarTurma(codTurma)
+    if(!t){
+      return false
+    }else {
+      return true
+      
+    }
+  }
+
+  const handleChange = (e) => {
+    setCodTurma(e.target.value);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const t = confirmarTurma()
+    const user = procurarPessoa()
+    if(t === false){
+      alert("Turma não encontrada!")
+    }else {
+      alert("Você já faz parte da turma!")
+      if(user.length > 0){
+      }
+    }
+    
   }
 
   return (
@@ -42,10 +106,13 @@ export const Home = () => {
           <ul class="card">
             <li className="card-imagem"><a href="#"><img class="banner-imagem" src="https://st.depositphotos.com/1002326/5133/v/450/depositphotos_51331625-stock-illustration-open-book-with-summer-landscape.jpg" alt="" /></a></li>
             <li className="card-item">
-              <button onClick={() => navigate('criar')}>Criar turma</button>
+              <Link to={'criar'}>Criar turma</Link>
             </li>
+            <form onSubmit={ handleSubmit }>
+              <input type="text" value={codTurma} required onChange={(e) => { handleChange(e) }} />
+              <button /*onClick={procura(codTurma)}*/ >Entrar</button>
+            </form>
           </ul>
-         
             {turmas.map((user) => {
               return (
                 <CardTurmaItem user={user} />
